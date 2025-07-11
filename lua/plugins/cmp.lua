dea74d00ea1b3f035c22ce33a4c312988ddaf61b
Acip <freshbone.acip@yahoo.co.id>
2025-07-11 11:23:50 +0800

    add phtml to c_css

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -168,7 +168,7 @@ return { -- Autocompletion
         {
           name = "c_css",
           option = {
-            enable_on = { "html", "vue", "php" }, -- html is enabled by default
+            enable_on = { "html", "vue", "php", "phtml" }, -- html is enabled by default
             notify = true,
             documentation = {
               auto_show = true, -- show documentation on select
